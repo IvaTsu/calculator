04cb9cc1901b5b8494a6d835fe1472b2e484d5cd
IvaTsu <ivan.tsud@gmail.com>
2018-11-14 17:57:53 +0200

    Add Unit Tests for 'add()' function

--- test.js
+++ test.js
@@ -1,11 +1,24 @@
 const assert = require("assert");
 
+const operations = require("./operations.js");
+const add = operations.add;
+
 // Test the test :)
 it('Should be TRUE', () => {
   assert.equal(true, true);
 });
 
-// test add() function
+// test add() function if numbers are passed
 it('Should return 4', () => {
   assert.equal(add(1, 3), 4);
+});
+
+// test add() function if numbers are passed as strings
+it('Should return 8', () => {
+  assert.equal(add('2', '6'), 8);
+});
+
+// test add() function if number and string as a number are passed
+it('Should return 10', () => {
+  assert.equal(add('5', 5), 10);
 });
