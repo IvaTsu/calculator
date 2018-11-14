07516e1faae11b5ba60f4c3d3052e84090f21710
IvaTsu <ivan.tsud@gmail.com>
2018-11-14 17:48:55 +0200

    Add a TDD test for not yet implemented add() function

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 const assert = require("assert");
 
+// Test the test :)
 it('Should be TRUE', () => {
   assert.equal(true, true);
+});
+
+// test add() function
+it('Should return 4', () => {
+  assert.equal(add(1, 3), 4);
 });
