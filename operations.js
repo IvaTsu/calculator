b763b973663afe057636973f88e85b219a5eccd7
IvaTsu <ivan.tsud@gmail.com>
2018-11-15 23:49:27 +0200

    Implement 'multiply()' and 'divide()' functions

--- operations.js
+++ operations.js
@@ -1,3 +1,6 @@
 const add = (x, y) => +x + +y;
 const subtract = (x, y) => +x - +y;
-module.exports = { add, subtract };
+const multiply = (x, y) => +x * +y;
+const divide = (x, y) => +x / +y;
+
+module.exports = { add, subtract, multiply, divide };
