645474f100c2c6cb10ea85e20b1f17ba2f46e32a
IvaTsu <ivan.tsud@gmail.com>
2018-11-15 23:11:14 +0200

    Add tests for 'subtract()' function

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const operations = require('./operations.js');
 
-const { add } = operations;
+const { add, subtract } = operations;
 
 // Test the test :)
 it('Should be TRUE', () => {
@@ -22,4 +22,24 @@ it('Should return 8', () => {
 // test add() function if number and string as a number are passed
 it('Should return 10', () => {
   assert.equal(add('5', 5), 10);
+});
+
+// test subtract() function if numbers are passed
+it('Should return 66', () => {
+  assert.equal(subtract(67, 1), 66);
+});
+
+// test subtract() function if numbers are passed as strings
+it('Should return 33', () => {
+  assert.equal(subtract('66', '33'), 33);
+});
+
+// test subtract() function if number and string as a number are passed
+it('Should return 10', () => {
+  assert.equal(subtract('60', 50), 10);
+});
+
+// test subtract() function if the output is negative
+it('Should return -1', () => {
+  assert.equal(subtract(30, 31), -1);
 });
